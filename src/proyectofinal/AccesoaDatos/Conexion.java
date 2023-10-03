e17d3c93a26d91e5cc95a05d884d659e6295784c
McGuinnis <141609613+McGuinnis@users.noreply.github.com>
2023-10-03 10:49:41 -0300

    Initial commit

---
+++ src/proyectofinal/AccesoaDatos/Conexion.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinal.AccesoaDatos;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Luis
+ */
+public class Conexion {
+
+    private static final String URL = "jdbc:mariadb://localhost:3306/";
+    private static final String DB = "paqueteturistico";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection;
+//
+
+    private Conexion() {
+    }
+
+    public static Connection getConexion() {
+        if (connection == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = java.sql.DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
+                JOptionPane.showMessageDialog(null, "conectado");
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "error al cargar los drivers");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse");
+            }
+        }
+        return connection;
+    }
+
+}
---
+++ src/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinal;
+
+import java.sql.Connection;
+import proyectofinal.AccesoaDatos.Conexion;
+
+/**
+ *
+ * @author Luis
+ */
+public class ProyectoFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Connection con = Conexion.getConexion();
+    
+    
+    }
+    
+}
