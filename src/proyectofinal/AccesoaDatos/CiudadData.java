3a78a183b090220e52f7fc0a2cbeccf78c1dcf39
OAaron11 <141650277+OAaron11@users.noreply.github.com>
2023-10-06 12:52:17 -0300

    ComboBox Paises y prueba de vistas

--- src/proyectofinal/AccesoaDatos/CiudadData.java
+++ src/proyectofinal/AccesoaDatos/CiudadData.java
@@ -22,7 +22,7 @@ import proyectofinal.Entidades.Ciudad;
  * @author Luis
  */
 public class CiudadData {
-    
+
     private PreparedStatement ps;
 
     private Connection con = null;
@@ -33,24 +33,24 @@ public class CiudadData {
     }
 
 //Insertando una Ciudad
-    public void agregarCiudad(Ciudad ciudad){
+    public void agregarCiudad(Ciudad ciudad) {
 
         String sql = "INSERT INTO ciudad (Nombre, Pais, Estado, Provincia)"
                 + "VALUES(?, ?, ?, ?)";
 
         try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            
+
             ps.setString(1, ciudad.getNombre());
             ps.setString(2, ciudad.getPais());
             ps.setBoolean(3, ciudad.isEstado());
             ps.setString(4, ciudad.getProvincia());
-            
+
             ps.executeUpdate();
-            
+
             ResultSet rs = ps.getGeneratedKeys();
-            
-            if (rs.next()){
+
+            if (rs.next()) {
                 ciudad.setIdCiudad(rs.getInt(1));
                 JOptionPane.showMessageDialog(null, "Ciudad Agregada con Exito");
             }
@@ -60,139 +60,137 @@ public class CiudadData {
         }
 
     }
-    
+
     public void eliminarCiudad(int id) { // ****Eliminando Logicamente una Ciudad
 
         String sql = "UPDATE ciudad SET Estado = 0 WHERE idCiudad = ?";
-        
+
         try {
             ps = con.prepareStatement(sql);
-            
+
             ps.setInt(1, id);
-            
+
             int exito = ps.executeUpdate();
-            
-            if (exito == 1){
-                
+
+            if (exito == 1) {
+
                 JOptionPane.showMessageDialog(null, "Ciudad eliminada con Exito");
             }
             ps.close();
-            
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos ciudad");
         }
     }
-    
+
     public void modificarCiudad(Ciudad ciudad) {
         String sql = "Update ciudad SET Nombre=?, Pais=?, Provincia=? "
                 + "Where idCiudad=?";
-        
-                
+
         try {
-            
+
             ps = con.prepareStatement(sql);
-            
+
             ps.setString(1, ciudad.getNombre());
             ps.setString(2, ciudad.getPais());
             ps.setString(3, ciudad.getProvincia());
             ps.setInt(4, ciudad.getIdCiudad());
-            
+
             int exito = ps.executeUpdate();
-            
-            if (exito == 1){
+
+            if (exito == 1) {
                 JOptionPane.showMessageDialog(null, "Ciudad actualizada con exito");
             }
-                        
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos ciudad");
         }
     }
 
-    public Ciudad buscarCiudad (int id){
-        Ciudad ciudad=null;
-        String sql="SELECT Nombre, Pais, Estado, Provincia FROM ciudad WHERE idCiudad=? ";
-        
+    public Ciudad buscarCiudad(int id) {
+        Ciudad ciudad = null;
+        String sql = "SELECT Nombre, Pais, Estado, Provincia FROM ciudad WHERE idCiudad=? ";
+
         try {
-            ps=con.prepareStatement(sql);
+            ps = con.prepareStatement(sql);
             ps.setInt(1, id);
-            ResultSet rs=ps.executeQuery();
-            
+            ResultSet rs = ps.executeQuery();
+
             if (rs.next()) {
-                ciudad= new Ciudad();
+                ciudad = new Ciudad();
                 ciudad.setIdCiudad(id);
                 ciudad.setNombre(rs.getString("Nombre"));
                 ciudad.setPais(rs.getString("Pais"));
                 ciudad.setEstado(rs.getBoolean("Estado"));
                 ciudad.setProvincia(rs.getString("Provincia"));
-                
-            }else{
+
+            } else {
                 JOptionPane.showMessageDialog(null, "Ciudad no Disponible");
             }
-            
-            ps.close();
-        } catch (SQLException ex) {
-             JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos ciudad");
-        }catch (NullPointerException ex2){
-            
-        }
-        return ciudad;
-    }
-    
-    public List<Ciudad> listarCiudadPorPais(String pais){
-        
-        String sql="SELECT `idCiudad`, `Nombre`, `Estado`, `Provincia` FROM `ciudad` WHERE estado=1 AND Pais=?";
-        ArrayList<Ciudad> ciudadList=new ArrayList<>();
-        try {
-            ps=con.prepareStatement(sql);
-            ps.setString(1, pais);
-            
-            
-            ResultSet rs=ps.executeQuery();
-            
-            while (rs.next()) {                
-                 Ciudad ciudad= new Ciudad();
-                 
-                 ciudad.setIdCiudad(rs.getInt("idCiudad"));
-                 ciudad.setNombre(rs.getString("Nombre"));
-                 ciudad.setEstado(true);
-                 ciudad.setProvincia(rs.getString("Provincia"));
-                 ciudad.setPais(pais); 
-                 ciudadList.add(ciudad);
-            }
-            
+
             ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos ciudad");
-        }finally{
-            try {
-            if (ps != null) {
-                ps.close();
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace(); 
+        } catch (NullPointerException ex2) {
+
         }
+        return ciudad;
+    }
+
+    public List<Ciudad> listarCiudadPorPais(String pais) {
+
+        String sql = "SELECT `idCiudad`, `Nombre`, `Estado`, `Provincia` FROM `ciudad` WHERE estado=1 AND Pais=?";
+        ArrayList<Ciudad> ciudadList = new ArrayList<>();
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pais);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Ciudad ciudad = new Ciudad();
+
+                ciudad.setIdCiudad(rs.getInt("idCiudad"));
+                ciudad.setNombre(rs.getString("Nombre"));
+                ciudad.setEstado(true);
+                ciudad.setProvincia(rs.getString("Provincia"));
+                ciudad.setPais(pais);
+                ciudadList.add(ciudad);
+            }
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos ciudad");
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
         return ciudadList;
     }
-    
-    public void listarPaises(){
-        String sql="SELECT DISTINCT pais FROM ciudad";
-  //      ArrayList<Ciudad> pais=new ArrayList<>();
-        
-        try {
-            ps=con.prepareStatement(sql);
-            ResultSet resultSet = ps.executeQuery(sql);
 
-            // Imprimir los países encontrados
-            while (resultSet.next()) {
-                String pais = resultSet.getString("pais");
-                System.out.println(pais);
+    public List<String> listarPaises() {
+
+        String sql = "SELECT DISTINCT pais FROM ciudad";
+        List<String> listaPaises = new ArrayList<>();
+
+        try {
+            ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                String pais = rs.getString("pais");
+                listaPaises.add(pais);
             }
         } catch (SQLException ex) {
-            Logger.getLogger(CiudadData.class.getName()).log(Level.SEVERE, null, ex);
+           JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos ciudad");
         }
-        
-    
-    
-    } 
+
+        return listaPaises;
+
+    }
 }
